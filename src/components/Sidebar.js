b7128388f5b86f510db2671b3198399c1536d86e
Roberto <hooverlds93@gmail.com>
2023-01-05 16:54:28 -0600

    tercer commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,19 @@ import '../styles/Side.css'
 const Sidebar = () => {
   return (
     <div className='container-sidebar'>
-        <div className='side'>InfoWeather</div>
+        
+        <div className='side'> 
+        <div>
+            <input />
+            <button>Search</button>
+        </div>
+            <img className='img-weather' src={require('../images/LightCloud.png')} alt=""/>            
+            <p><span className='grade'> 15</span>  °c</p>
+            <p className='clima'>Shower</p>
+            <p>Today . Fr 5 Jun</p>
+            <p>Ciudad de México</p>
+        </div>
+        
     </div>
   )  
 }
