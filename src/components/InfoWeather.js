96711cd551072a124bd8b38c14c03ccc33da2521
Roberto <hooverlds93@gmail.com>
2023-01-05 15:05:49 -0600

    first commit

---
+++ src/components/InfoWeather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Info.css'
+
+const InfoWeather = () => {
+  return (
+    <div className='container-infoweather'>
+        <div className='info'>InfoWeather</div>
+    </div>
+  )
+}
+
+export default InfoWeather
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Side.css'
+
+const Sidebar = () => {
+  return (
+    <div className='container-sidebar'>
+        <div className='side'>InfoWeather</div>
+    </div>
+  )  
+}
+
+export default Sidebar
